f6662a0382f9a336eef952aae4c91b743c9a6460
Andrea Belli <andrea.belli@gmail.com>
2024-10-11 06:20:42 +0200

    added comments

--- 6. Dashboards/d8_basic+callback.py
+++ 6. Dashboards/d8_basic+callback.py
@@ -1,16 +1,19 @@
 # -*- coding: utf-8 -*-
-
-
-
+'''
+Callbacks: functions that are automatically called by Dash whenever an input 
+component's property changes, in order to update some property in another 
+component (the output).
+'''
 
 import dash
 import dash_core_components as dcc
 import dash_html_components as html
 from dash.dependencies import Input, Output
 
-
+#app initialization
 app = dash.Dash()
 
+#layout section
 app.layout = html.Div(id = 'parent', children = [
     
     #heading 
@@ -36,6 +39,9 @@ app.layout = html.Div(id = 'parent', children = [
     
     ])
 
+
+# the callback takes as Input the dropdown list, and in particular, it's triggered by the value property
+# and return as Output a list of children components that will added to the output-text component.
 
 @app.callback(Output(component_id='output-text',component_property='children'),
               [Input(component_id='dropdown',component_property='value')])
